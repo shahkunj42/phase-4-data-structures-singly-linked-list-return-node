5c8b8123c4b25d83117045b82f912927378fe362
Kunj <shahkunj42@gmail.com>
2022-08-19 16:21:59 -0400

    commit tree 881515eed73966b2fa326b19cf1bf869b16bf683

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -8,7 +8,30 @@ class LinkedList
   end
 
   def nth_from_end(n)
-    # your code here
+    # determine the length of the list
+    length = 0
+    node = head
+    until node.nil?
+      node = node.next_node
+      length += 1
+    end
+
+    # exit early and return nil if n is not valid
+    return nil if n < 1 || n > length
+
+    # iterate based on the length of the list to find the node
+    # For example, if n = 2 and length is 4, length - n + 1 == 1
+    # 1 -> 2 -> 3 -> 4
+    #      i
+    node = head
+    i = 1
+    while i < length - n + 1
+      node = node.next_node
+      i += 1
+    end
+
+    # return the node's value
+    node.value
   end
 
 end
